8b3eec249988e40d630fe40954885eccd972e4f7
Sachin Dhawane <47055999+sachindhawane22@users.noreply.github.com>
2020-08-15 14:46:45 +0530

    adding initial changes

--- src/main/java/com/hackerrank/springsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/hackerrank/springsecurity/config/WebSecurityConfig.java
@@ -20,4 +20,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("{noop}admin_password")
                 .authorities("ROLE_OFFICE_ADMIN");
     }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/").hasAnyAuthority("ROLE_OFFICE_ADMIN","ROLE_STUDENT_USER")
+                .antMatchers(HttpMethod.POST,"/course").hasAuthority("ROLE_OFFICE_ADMIN")
+                .antMatchers(HttpMethod.POST,"/student").hasAnyAuthority("ROLE_OFFICE_ADMIN","ROLE_STUDENT_USER")
+                .anyRequest()
+                .authenticated()
+        .and()
+        .httpBasic();
+    }
 }
